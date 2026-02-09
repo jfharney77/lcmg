956f9cb3a41de8d2092ddee9f54c56a8a66d2a2e
John Harney <jfharney@gmail.com>
2026-02-09 16:33:53 -0500

    Test

--- script/run_all_a_models.bat
+++ script/run_all_a_models.bat
@@ -4,6 +4,6 @@ call "%~dp0config.bat"
 set SRC=%DEFAULT_PROJ_ROOT%/src
 set PYTHONPATH=%DEFAULT_PROJ_ROOT%\%SRC%
 
-echo Startiong a models...
+echo Starting a models...
 wt new-tab powershell -Command "uv run python %P_SERVER_PATH% %P_SERVER_PORT%" ; ^
    new-tab powershell -Command "uv run python %IT_SERVER_PATH% %IT_SERVER_PORT%"
