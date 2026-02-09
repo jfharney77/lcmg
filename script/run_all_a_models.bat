90d2b2254825b54d198d2277bc6a55ea648f5717
John Harney <jfharney@gmail.com>
2026-02-09 16:26:45 -0500

    WIP: run uv

--- script/run_all_a_models.bat
+++ script/run_all_a_models.bat
@@ -5,5 +5,5 @@ set SRC=%DEFAULT_PROJ_ROOT%/src
 set PYTHONPATH=%DEFAULT_PROJ_ROOT%\%SRC%
 
 echo Startiong a models...
-wt new-tab powershell -Command "python %P_SERVER_PATH% %P_SERVER_PORT%" ; ^
-   new-tab powershell -Command "python %IT_SERVER_PATH% %IT_SERVER_PORT%"
+wt new-tab powershell -Command "uv run python %P_SERVER_PATH% %P_SERVER_PORT%" ; ^
+   new-tab powershell -Command "uv run python %IT_SERVER_PATH% %IT_SERVER_PORT%"
